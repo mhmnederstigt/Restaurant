dbc830e3f5acf5915bf28fb69763c6e490164b08
mhmnederstigt <nederstigt.milou@gmail.com>
2018-05-09 11:48:13 +0200

    Updated main activity

--- app/src/main/java/com/example/gebruiker/restaurant/MainActivity.java
+++ app/src/main/java/com/example/gebruiker/restaurant/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.gebruiker.restaurant;
 
 import android.content.Intent;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,8 +13,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(MainActivity.this, CategoriesActivity.class);
-        startActivity(intent);
-        finish();
+        // set listener to entire layout
+        ConstraintLayout entireLayout = findViewById(R.id.welcome);
+        entireLayout.setOnClickListener(new WelcomeClickListener());
+    }
+
+    private class WelcomeClickListener implements View.OnClickListener {
+        // continue to menu when any place on the screen is clicked
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(MainActivity.this, CategoriesActivity.class);
+            startActivity(intent);
+            finish();
+        }
     }
 }
